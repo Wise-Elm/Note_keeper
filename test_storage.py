0e9ebc533abdd9563913003aa13aaa1ff8f94382
Wise-Elm <70621459+Wise-Elm@users.noreply.github.com>
2021-12-18 12:38:51 -0600

    Update test_storage.py

--- test_storage.py
+++ test_storage.py
@@ -20,7 +20,7 @@ class TestStorage(unittest.TestCase):
     """Perform unittest on storage.py."""
 
     def test_instantiate_templates(self):
-        """Test Repo._instantiate_templates() to make sure objects are being instantiated correctly."""
+        """Test Repo._instantiate_templates() to confirm objects are being instantiated correctly."""
 
         # Confirm that created objects are of the appropriate class.
         for objects, templates in repo.templates.items():
@@ -28,13 +28,13 @@ class TestStorage(unittest.TestCase):
                 self.assertIsInstance(template, repo.subclasses[objects])
 
     def test_id_duplicates(self):
-        """Test Repo._id to make sure it is not storing duplicate values."""
+        """Test Repo._id to confirm it is not storing duplicate values."""
 
         _id_2 = set(repo.id_)  # Make a set version of repo._id to eliminate possible duplicates.
         self.assertEqual(len(repo.id_), len(_id_2))
 
     def test_add_id(self):
-        """Test Repo._add_id to make sure that it will not add duplicate id's. or store id's of the wrong length"""
+        """Test Repo._add_id to confirm that it will not add duplicate id's. or store id's of the wrong length"""
 
         duplicate = random.choice(repo.id_)
         with self.assertRaises(StorageError):
